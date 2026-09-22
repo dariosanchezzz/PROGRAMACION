9bfaa956b037d241a18380236468b813e8c742ac
dariosanchezzz <sanchezmairenadario@gmail.com>
2026-09-22 14:10:01 +0200

    Add files via upload

---
+++ tema1/calleynumero.java
@@ -0,0 +1,5 @@
+public class calleynumero { 
+    public static void main(String[] args) { 
+        System.out.println("c/puerto y número: 787 54 32 54"); 
+    } 
+}
---
+++ tema1/dariosanchezmairena.java
@@ -0,0 +1,5 @@
+public class dariosanchezmairena { 
+    public static void main(String[] args) { 
+        System.out.println("Darío Sánchez Mairena!"); 
+    } 
+}
