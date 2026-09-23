e409c0ab0b95a4f054acc4019d14f3f05304eaf7
dariosanchezzz <sanchezmairenadario@gmail.com>
2026-09-23 08:12:06 +0200

    Add files via upload

---
+++ tema1/ejercicio4.java
@@ -0,0 +1,14 @@
+public class ejercicio4 { 
+    public static void main(String[] args) { 
+        System.out.println("Hello \t Hola ");
+        System.out.println("table \t mesa ");
+        System.out.println("face \t cara ");
+        System.out.println("car \t coche ");
+        System.out.println("house \t casa ");
+        System.out.println("problem \t problema ");
+        System.out.println("run \t correr ");
+        System.out.println("play \t jugar ");
+        System.out.println("go \t ir ");
+        System.out.println("school \t colegio ");
+    } 
+}
