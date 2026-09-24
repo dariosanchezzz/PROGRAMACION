a9ed12197970476c0e0e3ac4aa3aa893224e388b
dariosanchezzz <sanchezmairenadario@gmail.com>
2026-09-24 09:17:10 +0200

    Add files via upload

---
+++ tema1/horario.java
@@ -0,0 +1,19 @@
+public class horario {
+    public static void main(String[] args) { 
+        System.out.println("horas \t         Lunes \t Martes Miércoles Jueves Viernes");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("8:00/9:00 \t LMDAW \t BDDAW \t SIDAW \t PRDAW \t EDDAW");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("9:00/10:00 \t BDDAW \t SIDAW  PRDAW \t BDAWW\t BBDAW");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("10:00/11:00 \t SIDAW \t SOS \tITEC  \t LMDAW \t BDDAW");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("11:00/11:30 \trecreo  recreo  recreo \trecreo \t recreo");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("11:30/12:30 \t SIDAW \t EDDAW \t ITEC \t SIDAW \t ITEC");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("12:30/13:30 \t PRDAW \t PRDAW \t DIG \t BDDAW \t PRDAW");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("13:30/ 14:30 \t PRDAW \t PRDAW \t EDDAW \t BDDAW \tPRDAW");
+    } 
+}
